fb4d2f3c6ff1b986693ea47acaf84115a22e0fd6
keawade <keawade@gmail.com>
2015-09-17 18:53:32 -0500

    Bubble sort implemented

--- sorts.js
+++ sorts.js
@@ -3,3 +3,27 @@ var group2 = [32, 29, 28, 47, 50, 43, 40, 45, 37, 46, 27, 49, 35, 26, 34, 44, 48
 var group3 = ['sadly', 'roast', 'Loopy', 'burger', 'gavel', 'lure', 'postpone', 'frontal', 'Gavel', 'lavender'];
 
 // Your code goes here! Hooray!
+
+function bubbleSort( array ){
+  x = array.length;
+  var switched = true;
+  while( switched ){
+    switched = false;
+    for( var i = 0; i < x; i++ ){
+      if( array[i] > array[i+1] ){
+        var temp = array[i];
+        array[i] = array[i+1];
+        array[i+1] = temp;
+        switched = true;
+      }
+    }
+  }
+  return array;
+}
+
+function quickSort(array){
+  return array;
+}
+
+console.log(group1);
+console.log(bubbleSort(group1));
