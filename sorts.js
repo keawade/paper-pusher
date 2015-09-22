01000aaaed693d8617b8fb7be84ddbb3f7f37b7c
keawade <keawade@gmail.com>
2015-09-22 16:51:44 -0500

    ES6 and bubbleSort logic update

--- sorts.js
+++ sorts.js
@@ -1,20 +1,20 @@
-var group1 = [22, 3, 23, 17, 9, 18, 14, 11, 5, 25, 15, 10, 6, 19, 21, 4, 2, 13, 7, 16, 24, 12, 20, 1, 8];
-var group2 = [32, 29, 28, 47, 50, 43, 40, 45, 37, 46, 27, 49, 35, 26, 34, 44, 48, 42, 33, 41, 38, 31, 30, 36, 39];
-var group3 = ['sadly', 'roast', 'Loopy', 'burger', 'gavel', 'lure', 'postpone', 'frontal', 'Gavel', 'lavender'];
+"use strict";
+let group1 = [22, 3, 23, 17, 9, 18, 14, 11, 5, 25, 15, 10, 6, 19, 21, 4, 2, 13, 7, 16, 24, 12, 20, 1, 8];
+let group2 = [32, 29, 28, 47, 50, 43, 40, 45, 37, 46, 27, 49, 35, 26, 34, 44, 48, 42, 33, 41, 38, 31, 30, 36, 39];
+let group3 = ['sadly', 'roast', 'Loopy', 'burger', 'gavel', 'lure', 'postpone', 'frontal', 'Gavel', 'lavender'];
 
 // Your code goes here! Hooray!
 
 function bubbleSort( array ){
-  var a = array.slice(0);
+  let a = array.slice(0);
   if ( a.length == 0 ) {
     return [];
   }
-  var switched = true;
-  while( switched ){
+  for( let t = 0, switched = true, len = a.length; switched && t < len; t++){
     switched = false;
-    for( var i = 0; i < a.length; i++ ){
+    for( let i = 0; i < ( a.length - t ); i++ ){
       if( a[i] > a[i+1] ){
-        var b = a[i];
+        let b = a[i];
         a[i] = a[i+1];
         a[i+1] = b;
         switched = true;
@@ -24,15 +24,15 @@ function bubbleSort( array ){
   return a;
 }
 
-function quickSort(array){
-  var a = array.slice(0);
+function quickSort( array ){
+  let a = array.slice(0);
   if( a.length == 0 ) {
     return [];
   }
-  var left = [],
+  let left = [],
       pivot = a[0],
       right = []
-  for( var i = 1; i < a.length; i++ ){
+  for( let i = 1; i < a.length; i++ ){
     if( a[i] < pivot ){
       left.push(a[i]);
     } else {
@@ -46,6 +46,6 @@ console.log(bubbleSort(group1));
 console.log(bubbleSort(group2));
 console.log(bubbleSort(group3));
 
-console.log(quickSort(group1));
-console.log(quickSort(group2));
-console.log(quickSort(group3));
+//console.log(quickSort(group1));
+//console.log(quickSort(group2));
+//console.log(quickSort(group3));
